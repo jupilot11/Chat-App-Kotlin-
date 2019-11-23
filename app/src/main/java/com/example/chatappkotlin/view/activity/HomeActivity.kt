4b2625af2e99e11356af7c9f40c42483de27400b
jufielmac <lariosajupilot1@gmail.com>
2019-11-23 14:43:04 +0800

    Image Upload

--- app/src/main/java/com/example/chatappkotlin/view/activity/HomeActivity.kt
+++ app/src/main/java/com/example/chatappkotlin/view/activity/HomeActivity.kt
@@ -1,8 +1,20 @@
 package com.example.chatappkotlin.view.activity
 
+import android.content.Context
+import android.content.res.Configuration
+import android.opengl.Visibility
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.chatappkotlin.R
 import com.example.chatappkotlin.User
@@ -10,8 +22,11 @@ import com.example.chatappkotlin.util.Constants.Companion.INTENT_USER
 import com.example.chatappkotlin.view.fragment.HomeFragment
 import com.example.chatappkotlin.view.fragment.NotifactionFragment
 import com.example.chatappkotlin.view.fragment.ProfileFragment
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
 class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
@@ -21,6 +36,8 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
     var homeFragment: Fragment? = null
     var notifFragment: Fragment? = null
     var profileFragment: Fragment? = null
+    private var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+    var navigation_drawer_header: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,19 +49,44 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
 
         }
 
-
         homeFragment = HomeFragment.newInstance("", "")
         notifFragment = NotifactionFragment.newInstance("", "")
         profileFragment = ProfileFragment.newInstance("", "")
+
+
+        viewpagers = findViewById<ConstraintLayout>(R.id.viewpager)
+        tabLayouts = findViewById<TabLayout>(R.id.tablayout)
+
 
         val ft = supportFragmentManager.beginTransaction()
         ft.replace(R.id.framelayout, homeFragment as HomeFragment, "")
         ft.commit()
 
-        tablayout.addOnTabSelectedListener(this)
-        val badge = tablayout.getTabAt(2)?.orCreateBadge
+
+        navigation_drawer_header =
+            navigationView_main_menu.inflateHeaderView(R.layout.navigation_drawer_header)
+
+        setupDrawerContent(navigationView_main_menu)
+        actionBarDrawerToggle = setupDrawerToggle()
+
+        drawer_layout?.addDrawerListener(actionBarDrawerToggle!!)
+
+        tabLayouts.addOnTabSelectedListener(this)
+
+        val badge = tabLayouts.getTabAt(2)?.orCreateBadge
         badge?.isVisible = true
         badge?.number = 9
+
+
+
+        imageView3.visibility = View.GONE
+        drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+
+        imageView3.setOnClickListener {
+
+            drawer_layout.openDrawer(Gravity.RIGHT)
+        }
 
     }
 
@@ -55,15 +97,18 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
 
                 Toast.makeText(this@HomeActivity, "Tab 1 reselect", Toast.LENGTH_SHORT)
                     .show()
+
             }
             1 -> {
                 Toast.makeText(this@HomeActivity, "Tab 2 reselect", Toast.LENGTH_SHORT)
                     .show()
 
+
             }
             2 -> {
                 Toast.makeText(this@HomeActivity, "Tab 3 reselect", Toast.LENGTH_SHORT)
                     .show()
+
 
             }
         }
@@ -78,24 +123,120 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
 
             0 -> {
 
+                imageView3.visibility = View.GONE
+                drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
                 val ft = supportFragmentManager.beginTransaction()
                 ft.replace(R.id.framelayout, homeFragment as HomeFragment, "")
                 ft.commit()
 
+
             }
             1 -> {
+
+                imageView3.visibility = View.VISIBLE
+                drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+
 
                 val ft = supportFragmentManager.beginTransaction()
                 ft.replace(R.id.framelayout, profileFragment as ProfileFragment, "")
                 ft.commit()
+
+
             }
             2 -> {
+
+                imageView3.visibility = View.GONE
+                drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+
                 val ft = supportFragmentManager.beginTransaction()
                 ft.replace(R.id.framelayout, notifFragment as NotifactionFragment, "")
                 ft.commit()
+
+
+            }
+        }
+    }
+
+    companion object {
+
+        var viewpagers: ConstraintLayout? = null
+        lateinit var tabLayouts: TabLayout
+    }
+
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        return object : ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ) {
+            override fun onDrawerClosed(view: View) {
+                this@HomeActivity.supportInvalidateOptionsMenu()
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                this@HomeActivity!!.supportInvalidateOptionsMenu()
+                val imm =
+                    this@HomeActivity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(drawer_layout.applicationWindowToken, 0)
+            }
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                super.onDrawerSlide(drawerView, slideOffset)
+
+
+                viewpager.translationX = (-slideOffset) * (drawerView.width)
+                drawer_layout.bringChildToFront(drawerView)
+                drawer_layout.requestLayout()
             }
         }
     }
 
 
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { item ->
+            selectDrawerItem(item)
+            true
+        }
+    }
+
+
+    fun selectDrawerItem(menuItem: MenuItem) {
+        val fragment: Fragment? = null
+        val fragmentClass: Class<*>
+
+        menuItem.isChecked = false
+        when (menuItem.itemId) {
+
+            R.id.nav_saved -> {
+
+                Toast.makeText(this, "wewe", Toast.LENGTH_SHORT).show()
+
+            }
+            R.id.nav_settings -> {
+                Toast.makeText(this, "adasdas", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+        menuItem.isChecked = true
+        drawer_layout.closeDrawers()
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        actionBarDrawerToggle!!.onConfigurationChanged(newConfig)
+
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+
+        actionBarDrawerToggle!!.syncState()
+
+    }
 }
--- app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/chatappkotlin/view/fragment/ProfileFragment.kt
@@ -10,8 +10,10 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.drawerlayout.widget.DrawerLayout
 
 import com.example.chatappkotlin.R
+import com.example.chatappkotlin.view.activity.HomeActivity
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
@@ -24,8 +26,6 @@ private const val ARG_PARAM2 = "param2"
 class ProfileFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
-    private var actionBarDrawerToggle: ActionBarDrawerToggle? = null
-    var navigation_drawer_header: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,17 +44,6 @@ class ProfileFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        navigation_drawer_header =
-            view.navigationView_main_menu?.inflateHeaderView(R.layout.navigation_drawer_header)
-        setupDrawerContent(view.navigationView_main_menu)
-
-        actionBarDrawerToggle = setupDrawerToggle()
-        view.drawer_layout.addDrawerListener(actionBarDrawerToggle!!)
-
-        view.imageView3.setOnClickListener {
-
-            view.drawer_layout.openDrawer(Gravity.RIGHT)
-        }
 
         return view
     }
@@ -70,73 +59,16 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    private fun setupDrawerToggle(): ActionBarDrawerToggle {
-        return object : ActionBarDrawerToggle(
-            activity,
-            drawer_layout,
-            toolbar,
-            R.string.drawer_open,
-            R.string.drawer_close
-        ) {
-            override fun onDrawerClosed(view: View) {
-                activity!!.supportInvalidateOptionsMenu()
-            }
-
-            override fun onDrawerOpened(drawerView: View) {
-                activity!!.supportInvalidateOptionsMenu()
-                val imm =
-                    activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(drawer_layout.applicationWindowToken, 0)
-            }
-
-            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-                super.onDrawerSlide(drawerView, slideOffset)
-                constraintLayout.translationX = (-slideOffset) * (drawerView.width)
-                drawer_layout.bringChildToFront(drawerView)
-                drawer_layout.requestLayout()
-            }
-        }
-    }
-
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        actionBarDrawerToggle!!.onConfigurationChanged(newConfig)
 
     }
 
 
-    private fun setupDrawerContent(navigationView: NavigationView) {
-        navigationView.setNavigationItemSelectedListener { item ->
-            selectDrawerItem(item)
-            true
-        }
-    }
 
 
-    fun selectDrawerItem(menuItem: MenuItem) {
-        val fragment: Fragment? = null
-        val fragmentClass: Class<*>
 
-        menuItem.isChecked = false
-        when (menuItem.itemId) {
 
-            R.id.nav_saved ->{
 
-                Toast.makeText(activity,"wewe",Toast.LENGTH_SHORT).show()
 
-            }
-            R.id.nav_settings ->{
-                Toast.makeText(activity,"adasdas",Toast.LENGTH_SHORT).show()
-
-            }
-
-        }
-        menuItem.isChecked = true
-        drawer_layout.closeDrawers()
-    }
 
 
 }
